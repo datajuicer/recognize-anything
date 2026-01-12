889201b76458513d04afde5d4cfa376aa9e33ebf
Yilun Huang <hyl1024@yeah.net>
2026-01-12 20:47:09 +0800

    Load requirements from requirements.txt for setup explicitly

--- setup.py
+++ setup.py
@@ -1,2 +1,6 @@
 import setuptools
-setuptools.setup()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
+setuptools.setup(install_requires=requirements)
